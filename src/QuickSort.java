840462c8bc8ab0df9f1ec42d28af2eaba56cb995
FatemehRajabi <100088087+FatemehRajabi@users.noreply.github.com>
2025-06-11 12:24:32 -0400

    update quick sort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -13,11 +13,17 @@ public class QuickSort {
     }
 
     private int partition(int[] array, int low, int high){
+
+        int randomIndex = low + (int)(Math.random() * (high - low + 1));
+        int temporary = array[randomIndex];
+        array[randomIndex] = array[high];
+        array[high] = temporary;
+
         int pivot = array[high];
         int i = low - 1;
 
         for(int j=low; j<high; j++){
-            if(array[j] < pivot){
+            if(array[j] <= pivot){
                 i++;
                 int temp = array[i];
                 array[i] = array[j];
@@ -32,3 +38,54 @@ public class QuickSort {
     }
 }
 
+//
+//
+//import java.util.Random;
+//
+//public class QuickSort {
+//    private Random random = new Random();
+//
+//    public void quickSort(int[] array) {
+//        if (array == null || array.length <= 1) return;
+//        quickSort(array, 0, array.length - 1);
+//    }
+//
+//    private void quickSort(int[] array, int low, int high) {
+//        if (low < high) {
+//            // Randomize pivot selection
+//            randomizePivot(array, low, high);
+//
+//            int pivotIndex = partition(array, low, high);
+//            quickSort(array, low, pivotIndex - 1);
+//            quickSort(array, pivotIndex + 1, high);
+//        }
+//    }
+//
+//    private void randomizePivot(int[] array, int low, int high) {
+//        // Generate random index between low and high (inclusive)
+//        int randomIndex = low + random.nextInt(high - low + 1);
+//
+//        // Swap random element with last element (which partition uses as pivot)
+//        swap(array, randomIndex, high);
+//    }
+//
+//    private int partition(int[] array, int low, int high) {
+//        int pivot = array[high]; // Now this is a randomized element
+//        int i = low - 1;
+//
+//        for (int j = low; j < high; j++) {
+//            if (array[j] <= pivot) {
+//                i++;
+//                swap(array, i, j);
+//            }
+//        }
+//        swap(array, i + 1, high);
+//        return i + 1;
+//    }
+//
+//    private void swap(int[] array, int i, int j) {
+//        int temp = array[i];
+//        array[i] = array[j];
+//        array[j] = temp;
+//    }
+//}
--- src/SortTest.java
+++ src/SortTest.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class SortTest {
@@ -6,6 +7,7 @@ public class SortTest {
         MergeSort mergeSort = new MergeSort();
         HeapSort heapSort = new HeapSort();
         QuickSort quickSort = new QuickSort();
+        ModifiedQuickSort modifiedQuickSort = new ModifiedQuickSort();
 
         Random random = new Random();
 
@@ -20,6 +22,17 @@ public class SortTest {
                 arrayToSort[i] = random.nextInt();
             }
 
+            int [] sortedArray = arrayToSort.clone();
+            Arrays.sort(sortedArray);
+
+            int[] reversedArray = arrayToSort.clone();
+            Arrays.sort(reversedArray);
+            for (int i = 0; i < reversedArray.length / 2; i++) {
+                int temp = reversedArray[i];
+                reversedArray[i] = reversedArray[reversedArray.length - 1 - i];
+                reversedArray[reversedArray.length - 1 - i] = temp;
+            }
+//----------------------------------------------------------------------------------------------------------------------
             // Insertion Sort
             long insertionTotal = 0;
             for (int i = 0; i < 5; i++) {
@@ -31,6 +44,28 @@ public class SortTest {
             }
             System.out.println("Insertion Sort: " + (insertionTotal / 5) + " ns");
 
+            insertionTotal = 0;
+            for (int i = 0; i < 5; i++) {
+                int[] clone = sortedArray.clone();
+                long start = System.nanoTime();
+                insertionSort.insertionSort(clone);
+                long end = System.nanoTime();
+                insertionTotal += (end - start);
+            }
+            System.out.println("(sorted input) Insertion Sort: " + (insertionTotal / 5) + " ns");
+
+            insertionTotal = 0;
+            for (int i = 0; i < 5; i++) {
+                int[] clone = reversedArray.clone();
+                long start = System.nanoTime();
+                insertionSort.insertionSort(clone);
+                long end = System.nanoTime();
+                insertionTotal += (end - start);
+            }
+            System.out.println("(reversed input) Insertion Sort: " + (insertionTotal / 5) + " ns");
+
+
+//----------------------------------------------------------------------------------------------------------------------
             // Merge Sort
             long mergeTotal = 0;
             for (int i = 0; i < 5; i++) {
@@ -42,6 +77,27 @@ public class SortTest {
             }
             System.out.println("Merge Sort: " + (mergeTotal / 5) + " ns");
 
+            mergeTotal = 0;
+            for (int i = 0; i < 5; i++) {
+                int[] clone = sortedArray.clone();
+                long start = System.nanoTime();
+                mergeSort.mergeSort(clone);
+                long end = System.nanoTime();
+                mergeTotal += (end - start);
+            }
+            System.out.println("(sorted input) Merge Sort: " + (mergeTotal / 5) + " ns");
+
+            mergeTotal = 0;
+            for (int i = 0; i < 5; i++) {
+                int[] clone = reversedArray.clone();
+                long start = System.nanoTime();
+                mergeSort.mergeSort(clone);
+                long end = System.nanoTime();
+                mergeTotal += (end - start);
+            }
+            System.out.println("(reversed input) Merge Sort: " + (mergeTotal / 5) + " ns");
+
+//----------------------------------------------------------------------------------------------------------------------
             // Heap Sort
             long heapTotal = 0;
             for (int i = 0; i < 5; i++) {
@@ -53,6 +109,27 @@ public class SortTest {
             }
             System.out.println("Heap Sort: " + (heapTotal / 5) + " ns");
 
+            heapTotal = 0;
+            for (int i = 0; i < 5; i++) {
+                int[] clone = sortedArray.clone();
+                long start = System.nanoTime();
+                heapSort.heapSort(clone);
+                long end = System.nanoTime();
+                heapTotal += (end - start);
+            }
+            System.out.println("(sorted input) Heap Sort: " + (heapTotal / 5) + " ns");
+
+            heapTotal = 0;
+            for (int i = 0; i < 5; i++) {
+                int[] clone = reversedArray.clone();
+                long start = System.nanoTime();
+                heapSort.heapSort(clone);
+                long end = System.nanoTime();
+                heapTotal += (end - start);
+            }
+            System.out.println("(reversed input) Heap Sort: " + (heapTotal / 5) + " ns");
+
+//----------------------------------------------------------------------------------------------------------------------
             // Quick Sort
             long quickTotal = 0;
             for (int i = 0; i < 5; i++) {
@@ -63,6 +140,60 @@ public class SortTest {
                 quickTotal += (end - start);
             }
             System.out.println("Quick Sort: " + (quickTotal / 5) + " ns");
+
+            quickTotal = 0;
+            for (int i = 0; i < 5; i++) {
+                int[] clone = sortedArray.clone();
+                long start = System.nanoTime();
+                quickSort.quickSort(clone);
+                long end = System.nanoTime();
+                quickTotal += (end - start);
+            }
+            System.out.println("(sorted input) Quick Sort: " + (quickTotal / 5) + " ns");
+
+            quickTotal = 0;
+            for (int i = 0; i < 5; i++) {
+                int[] clone = reversedArray.clone();
+                long start = System.nanoTime();
+                quickSort.quickSort(clone);
+                long end = System.nanoTime();
+                quickTotal += (end - start);
+            }
+            System.out.println("(reversed input) Quick Sort: " + (quickTotal / 5) + " ns");
+
+
+//----------------------------------------------------------------------------------------------------------------------
+            // Modified Quick Sort
+            long modifiedQuickTotal = 0;
+            for (int i = 0; i < 5; i++) {
+                int[] clone = arrayToSort.clone();
+                long start = System.nanoTime();
+                modifiedQuickSort.modifiedQuickSort(clone);
+                long end = System.nanoTime();
+                modifiedQuickTotal += (end - start);
+            }
+            System.out.println("Modified Quick Sort: " + (modifiedQuickTotal / 5) + " ns");
+
+            modifiedQuickTotal = 0;
+            for (int i = 0; i < 5; i++) {
+                int[] clone = sortedArray.clone();
+                long start = System.nanoTime();
+                modifiedQuickSort.modifiedQuickSort(clone);
+                long end = System.nanoTime();
+                modifiedQuickTotal += (end - start);
+            }
+            System.out.println("(sorted input) Modified Quick Sort: " + (modifiedQuickTotal / 5) + " ns");
+
+            modifiedQuickTotal = 0;
+            for (int i = 0; i < 5; i++) {
+                int[] clone = reversedArray.clone();
+                long start = System.nanoTime();
+                modifiedQuickSort.modifiedQuickSort(clone);
+                long end = System.nanoTime();
+                modifiedQuickTotal += (end - start);
+            }
+            System.out.println("(reversed input) Modified Quick Sort: " + (modifiedQuickTotal / 5) + " ns");
+
         }
     }
 }
